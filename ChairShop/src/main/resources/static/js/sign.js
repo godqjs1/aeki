ded39fb4c2d174bc2d03e6fb70ae0ee0e0a4737f
godqjs1 <godqjs1@gmail.com>
2024-03-27 01:05:16 +0900

    sign 수정

--- ChairShop/src/main/resources/static/js/sign.js
+++ ChairShop/src/main/resources/static/js/sign.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 	
-const signUpBtn = document.getElementById("signUp");
-const signInBtn = document.getElementById("signIn");
-const container = document.querySelector(".container");
-
-signUpBtn.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
-});
-signInBtn.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
-});
+	const signUpBtn = document.getElementById("signUp");
+	const signInBtn = document.getElementById("signIn");
+	const container = document.querySelector(".container");
+	
+	signUpBtn.addEventListener("click", () => {
+	  container.classList.add("right-panel-active");
+	});
+	signInBtn.addEventListener("click", () => {
+	  container.classList.remove("right-panel-active");
+	});
 });
