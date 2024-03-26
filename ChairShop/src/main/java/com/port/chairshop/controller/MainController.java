dd8dc3909b420031dcdb33d85915c5e2721581d3
godqjs1 <godqjs1@gmail.com>
2024-03-26 23:52:15 +0900

    깃

---
+++ ChairShop/src/main/java/com/port/chairshop/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.port.chairshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class MainController {
+
+	@GetMapping("/sign")
+	public void sign() {		
+	}
+	
+	@GetMapping("/cart")
+	public void cart() {		
+	}
+	
+}
