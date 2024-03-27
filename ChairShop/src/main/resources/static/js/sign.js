ba893965b371a92c98bee98fb9029e0ffa6dafa8
agimango <bsh7464@gmail.com>
2024-03-27 23:30:06 +0900

    header include

--- ChairShop/src/main/resources/static/js/sign.js
+++ ChairShop/src/main/resources/static/js/sign.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	
 	const signUpBtn = document.getElementById("signUp");
 	const signInBtn = document.getElementById("signIn");
-	const container = document.querySelector(".container");
+	const container = document.querySelector(".signContainer");
 	
 	signUpBtn.addEventListener("click", () => {
 	  container.classList.add("right-panel-active");
